b676b9b267eaf84724f4493ea2274f6361a44fbb
Michael Eisenbraun <eisenbm@algonquincollege.com>
2026-03-15 16:08:29 -0400

    Creates starter files for Part 2

---
+++ resources/views/components/layout/container.blade.php
@@ -0,0 +1,3 @@
+<section class="p-8 max-w-7xl">
+   {{ $slot }}
+</section>
---
+++ resources/views/components/layout/grid.blade.php
@@ -0,0 +1,3 @@
+<section class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 content-start py-4">
+    {{ $slot }}
+</section>
